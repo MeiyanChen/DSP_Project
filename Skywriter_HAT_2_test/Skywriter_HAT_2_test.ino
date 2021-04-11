37552d2cabbd0c489d372e7e74598ed0bab4c89a
MeiyanChen <chen445718310@gmail.com>
2021-04-11 15:51:55 +0200

    read data from arduino to supercollider

---
+++ Skywriter_HAT_2_test/Skywriter_HAT_2_test.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include "skywriter.h"
+
+#define PIN_TRFR  0   
+#define PIN_RESET 3
+
+void setup() {
+  Serial.begin(9600); //how many bit per second
+  while(!Serial){};
+  Serial.println("Hello World!");
+  
+  Skywriter.begin(PIN_TRFR, PIN_RESET);
+  Skywriter.onXYZ(handle_xyz);
+}
+
+void loop() {
+  Skywriter.poll();
+}
+
+void handle_xyz(unsigned int x, unsigned int y, unsigned int z){
+ char buf[17];
+ sprintf(buf, "%05u:%05u:%05u", x, y, z);  //
+ Serial.println(buf);
+}
